41d15f463dceb6e3e43abf2f04084aef69dbe264
Agus <aguskenny24@gmail.com>
2023-10-26 15:13:04 -0300

    - Added commit request using octokit - Changed port because I like using 3000 for the front end - Removed some types

--- takehomeapi/src/app/app.service.ts
+++ takehomeapi/src/app/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Octokit } from 'octokit';
+// Didn't use a token because it is not needed for public repos
+const octokit = new Octokit();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    try {
+      const response = await octokit.request(
+        'GET /repos/{owner}/{repo}/commits',
+        {
+          owner: 'Idontplaytheguitar',
+          repo: 'TechTestFSF',
+        },
+      );
+      return response;
+    } catch (error) {
+      return error;
+    }
   }
 }
