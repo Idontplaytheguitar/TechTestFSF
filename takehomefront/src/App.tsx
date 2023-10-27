56931ba3644b7a2994de0924633b0d559b072c7d
Agus <aguskenny24@gmail.com>
2023-10-27 18:49:00 -0300

    - Fixing shadcnUI - Using Tabs Example

--- takehomefront/src/App.tsx
+++ takehomefront/src/App.tsx
@@ -1,5 +1,14 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../@/components/ui/tabs"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <Tabs defaultValue="account" className="w-[400px]">
+    <TabsList>
+      <TabsTrigger value="account">Account</TabsTrigger>
+      <TabsTrigger value="password">Password</TabsTrigger>
+    </TabsList>
+    <TabsContent value="account">Make changes to your account here.</TabsContent>
+    <TabsContent value="password">Change your password here.</TabsContent>
+  </Tabs>
   );
 }
