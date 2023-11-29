46f2379092b8a9e59b4af87cf6af9c85e4d1fdfb
Agus <aguskenny24@gmail.com>
2023-11-29 20:05:14 -0300

    - Using env

--- takehomefront/src/utils/getCommits.ts
+++ takehomefront/src/utils/getCommits.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 export async function getCommits(owner: string, repo: string) {
   try {
     const response = await axios.get(
-      `http://localhost:3001/${owner}/${repo}`,
+      `${process.env.API}/${owner}/${repo}`,
     );
     return response.data;
   } catch (error) {
