b4be49818a80d6cf16a815d4fcca255644c21bde
Agus <aguskenny24@gmail.com>
2023-11-29 20:16:09 -0300

    - Changed how env is imported

--- takehomefront/src/utils/getCommits.ts
+++ takehomefront/src/utils/getCommits.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 export async function getCommits(owner: string, repo: string) {
   try {
     const response = await axios.get(
-      `${process.env.API}/${owner}/${repo}`,
+      `${import.meta.env.VITE_API}/${owner}/${repo}`,
     );
+    console.log(response);
     return response.data;
   } catch (error) {
     return error;
